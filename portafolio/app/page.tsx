68cd17d19c07af9bc571e73b0de9d62a048fb87d
BrunoAG2610 <145497948+BrunoAG2610@users.noreply.github.com>
2026-04-17 22:06:50 +0000

    Projects cambios

--- portafolio/app/page.tsx
+++ portafolio/app/page.tsx
@@ -3,6 +3,7 @@ import Header from "./Components/Header/Header";
 import Hero from "./Components/Hero/Hero";
 import Interests_Subjects from "./Components/Interests_Subjects/Interests_Subjects";
 import CV from "./Components/CV/CV";
+import Projects from "./Components/Projects/Projects";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
     <Hero />
     <Interests_Subjects />
     <CV />
+    <Projects />
     </>
   );
 }
