a7cae5a804cf1decbe912e89e26829337c566902
BrunoAG2610 <145497948+BrunoAG2610@users.noreply.github.com>
2026-04-17 20:43:22 +0000

    Interests and subjects cambios

--- portafolio/app/page.tsx
+++ portafolio/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Header from "./Components/Header/Header";
 import Hero from "./Components/Hero/Hero";
+import Interests_Subjects from "./Components/Interests_Subjects/Interests_Subjects";
 
 export default function Home() {
   return (
     <>
     <Header />
     <Hero />
+    <Interests_Subjects />
     </>
   );
 }
